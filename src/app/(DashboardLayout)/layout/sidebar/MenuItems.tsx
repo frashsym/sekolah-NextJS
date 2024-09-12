e4316a17d3a9e9012f933710adeed2f55ad45a73
frashsym <rfqfrashsym@gmail.com>
2024-09-12 15:54:00 +0700

    lanjut besok

--- src/app/(DashboardLayout)/layout/sidebar/MenuItems.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/MenuItems.tsx
@@ -1,7 +1,19 @@
 import {
-  IconBoxMultiple, IconCircleDot, IconHome, IconInfoCircle, IconLayout, IconLayoutGrid, IconPhoto, IconPoint, IconStar, IconTable, IconUser
+  IconBoxMultiple,
+  IconCircleDot,
+  IconHome,
+  IconInfoCircle,
+  IconLayout,
+  IconLayoutGrid,
+  IconPhoto,
+  IconPoint,
+  IconStar,
+  IconTable,
+  IconUser,
 } from "@tabler/icons-react";
-import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoNewspaperOutline } from "react-icons/io5";
+import { RiAdvertisementLine } from "react-icons/ri";
 
 import { uniqueId } from "lodash";
 
@@ -14,47 +26,82 @@ const Menuitems = [
   },
   {
     id: uniqueId(),
-    title: "Buttons",
-    icon: IconCircleDot,
-    href: "/ui-components/buttons",
-  },
-  {
-    id: uniqueId(),
-    title: "Forms",
-    icon: IconTable,
-    href: "/ui-components/forms",
-  },
-  {
-    id: uniqueId(),
-    title: "Alerts",
-    icon: IconInfoCircle,
-    href: "/ui-components/alerts",
-  },
-  {
-    id: uniqueId(),
-    title: "Ratings",
-    icon: IconStar,
-    href: "/ui-components/ratings",
-  },
-  {
-    id: uniqueId(),
-    title: "Images",
-    icon: IconPhoto,
+    title: "Menu Utama",
+    icon: GiHamburgerMenu,
     href: "#", // Tambahkan ini
     children: [
-      { id: uniqueId(), href: "/pages/absen/data-absensi", name: "Data Absensi" },
-      { id: uniqueId(), href: "/pages/absen/geolocation", name: "Geolocation" },
+      // Cuma buat ngetes bisa ngga
+      {
+        id: uniqueId(),
+        name: "Identitas Website",
+        href: "/menu-utama/identitas-website",
+      },
+      { id: uniqueId(), name: "Menu Website", href: "/menu-utama/menu-utama" },
+      {
+        id: uniqueId(),
+        name: "Halaman Baru",
+        href: "/menu-utama/halaman-baru",
+      },
     ],
   },
   {
     id: uniqueId(),
-    title: "Pagination",
+    title: "Modul Berita",
+    icon: IoNewspaperOutline,
+    href: "#", // Tambahkan ini
+    children: [
+      // Cuma buat ngetes bisa ngga
+      {
+        id: uniqueId(),
+        name: "Identitas Website",
+        href: "/menu-utama/identitas-website",
+      },
+      { id: uniqueId(), name: "Menu Website", href: "/menu-utama/menu-utama" },
+      {
+        id: uniqueId(),
+        name: "Halaman Baru",
+        href: "/menu-utama/halaman-baru",
+      },
+    ],
+  },
+  {
+    id: uniqueId(),
+    title: "Modul Iklan",
+    icon: RiAdvertisementLine,
+    href: "/ui-components/alerts",
+  },
+  {
+    id: uniqueId(),
+    title: "Modul Interaksi",
+    icon: IconPhoto,
+    href: "#", // Tambahkan ini
+    children: [
+      // Cuma buat ngetes bisa ngga
+      { id: uniqueId(), href: "/menu-utama", name: "Data Absensi" },
+      { id: uniqueId(), href: "/ui-components/images", name: "Geolocation" },
+    ],
+  },
+  {
+    id: uniqueId(),
+    title: "Modul Sekolah",
     icon: IconUser,
     href: "/ui-components/pagination",
   },
   {
     id: uniqueId(),
-    title: "Tables",
+    title: "Modul Users",
+    icon: IconLayoutGrid,
+    href: "/ui-components/table",
+  },
+  {
+    id: uniqueId(),
+    title: "Modul Video",
+    icon: IconLayoutGrid,
+    href: "/ui-components/table",
+  },
+  {
+    id: uniqueId(),
+    title: "Modul Web",
     icon: IconLayoutGrid,
     href: "/ui-components/table",
   },
